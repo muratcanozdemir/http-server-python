4b0ab81bd158b3fbaef6bedaad029ac3418cea2d
Murat Can Ozdemir <can@reynencourt.com>
2024-06-24 20:05:03 +0200

    user agent

--- app/main.py
+++ app/main.py
@@ -19,11 +19,26 @@ def main():
             parts = request_line.split()
             if len(parts) > 1:
                 url_path = parts[1]
+                headers = {}
+                for line in lines[1:]:
+                    if line:
+                        header_name, header_value = line.split(":", 1)
+                        headers[header_name.strip()] = header_value.strip()
                 if url_path == '/':
                     response = "HTTP/1.1 200 OK\r\n\r\n"
                 elif url_path.startswith("/echo/"):
                     echo_str = url_path[len("/echo/"):]
                     response_body = echo_str
+                    content_length = len(response_body)
+                    response = (
+                        "HTTP/1.1 200 OK\r\n"
+                        f"Content-Type: text/plain\r\n"
+                        f"Content-Length: {content_length}\r\n\r\n"
+                        f"{response_body}"
+                    )
+                elif url_path == "/user-agent":
+                    user_agent = headers.get("User-Agent", "Unknown")
+                    response_body = user_agent
                     content_length = len(response_body)
                     response = (
                         "HTTP/1.1 200 OK\r\n"
