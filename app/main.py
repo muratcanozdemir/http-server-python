1189486a7d6410cd97ecff7a7f65a12c2b5d687b
Murat Can Ozdemir <can@reynencourt.com>
2024-06-24 19:47:23 +0200

    return 200

--- app/main.py
+++ app/main.py
@@ -11,6 +11,18 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
 
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f{"Connection from {client_address}"})
+
+        request = client_socket.recv(1024)
+        print(f{"Received request: {request}"})
+
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+        client_socket.sendall(response.encode())
+
+        client_socket.close()
+
 
 if __name__ == "__main__":
     main()
